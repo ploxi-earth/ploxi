0a9e959880b9aecb25e1d0f4a6bce58edf57642d
AlchemyNitin <nitin222764@gmail.com>
2026-01-26 13:57:47 +0530

    Google analytics snippet

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { CartProvider } from '@/contexts/CartContext'
 import ConditionalNavbar from '@/components/layout/ConditionalNavbar'
 import './globals.css'
+import Script from "next/script";
 
 export const metadata = {
   manifest: '/site.webmanifest',
@@ -21,6 +22,21 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RLR7SYE246"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RLR7SYE246');
+          `}
+        </Script>
+      </head>
       <body>
         <CartProvider>
           <ConditionalNavbar />
